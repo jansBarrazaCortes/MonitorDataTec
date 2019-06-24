0166a38b3b6b6dd6d1815131efd09d1ced914e66
jansBarrazaCortes <jans.barraza.c@gmail.com>
2019-06-24 17:41:02 -0400

    monitor

---
+++ MonitorDataTec/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MonitorDataTec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 99);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ MonitorDataTec/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MonitorDataTec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            FileSystemWatcher observador = new FileSystemWatcher(@"C:\Temporal");
+            observador.NotifyFilter = (
+                NotifyFilters.LastAccess |
+                NotifyFilters.LastWrite |
+                NotifyFilters.FileName |
+                NotifyFilters.DirectoryName);
+
+
+            observador.Changed += Alcambiar;
+
+            observador.EnableRaisingEvents = true;
+
+
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            this.ShowInTaskbar = false;
+        }
+        private void Alcambiar(object source, FileSystemEventArgs e)
+        {
+
+            WatcherChangeTypes TipoCambio = e.ChangeType;
+         
+            if (e.Name.Equals("Data-Tec.txt"))
+            {
+                notifyIcon1.BalloonTipText = "El Archivo " + e.Name + " Fue Modificado";
+                notifyIcon1.ShowBalloonTip(100);
+            }
+         
+
+          
+
+        }
+
+    }
+}
